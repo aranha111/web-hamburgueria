a9fb9135062fc67e01fd9ee298a84d99b3b04ce3
aranha111 <ikazuna19.10@gmail.com>
2025-11-16 09:59:47 -0300

    corrige nome das imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import { CartProvider } from './assets/context/CartContext';
-import  Header  from './assets/components/Header';
-import  Cart  from './assets/components/Cart';
+import Header from './assets/components/Header';
+import Cart from './assets/components/Cart';
 import { ProductCard } from './assets/components/ProductCard';
 import './App.css';
+
 
 function App() {
   return (
     <CartProvider>
       <Header />
-      <main className ="flex flex-wrap justify-center gap-6 p-6">
-        <ProductCard
-         name="Hamburguer"
-            price={5.9}
-           img="src/assets/images/Hamburguerweb.png"
+      <main className="flex flex-wrap justify-center gap-6 p-6">
 
-          />
-          <ProductCard
-            name="Duplo Bacon"
-            price={19.9}
-            img="src/assets/Images/duplo-bacon.png"
-          />
-          <ProductCard
-            name="Chicken Burger"
-            price={17.5}
-            img="src/assets/Images/frango.png"
-            />
-        < Cart />
+      <ProductCard
+  name="Hamburguer"
+  price={5.9}
+  img="/hamburgueria-web/Images/hamburguerweb.png"
+/>
+
+<ProductCard
+  name="Duplo Bacon"
+  price={19.9}
+  img="/hamburgueria-web/Images/duplo-bacon.png"
+/>
+
+<ProductCard
+  name="Chicken Burger"
+  price={17.5}
+  img="/hamburgueria-web/Images/frango.png"
+/>
+
+
+        <Cart />
       </main>
     </CartProvider>
   );
